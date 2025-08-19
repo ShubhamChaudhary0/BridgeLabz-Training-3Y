7ab95dd74d002ed8b786eb0f7326c051b8d4d270
ShubhamChaudhary0 <chaudharyshubham1947@gmail.com>
2025-08-19 11:30:37 +0530

    added

--- JavaFundamental/CelsiustoFahrenheitConversion.java
+++
@@ -1,9 +0,0 @@
-package JavaFundamental;
-
-public class CelsiustoFahrenheitConversion {
-    public static void main(String[] args) {
-        double celsius = 25.0;
-        double fahrenheit = (celsius * 9/5) + 32;
-        System.out.println(celsius + "°C is equal to " + fahrenheit + "°F");
-    }
-}
